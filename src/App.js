b88393720c832cbcae8a346b53455098b10401ea
henoksemereab <162745729+henoksemereab@users.noreply.github.com>
2024-05-10 12:32:41 -0700

    Header

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Header from "./Components/Header/Header";
+import Home from "./Pages/Home/Home";
+
+function App() {
+	return (
+		<div className="App">
+			{/* <Home /> */}
+
+      <Header />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "../../Components/Header/Header";
+// import Header from '../../Components/Header/Header'
+// import Footer from '../../Components/Footer/Footer'
+// import Banner from '../../Components/Banner/Banner'
+// import RowList from '../../Components/Rows/RowList/RowList'
+
+const Home = () => {
+	return (
+		<>
+			{/* <Header /> */}
+			{/* <Header/> */}
+			{/* <Banner/>
+     <RowList/> 
+     <Footer/> */}
+		</>
+	);
+};
+
+export default Home;
